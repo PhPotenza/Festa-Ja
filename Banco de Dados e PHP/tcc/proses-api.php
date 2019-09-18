97b676b32c8d6a10075864d3379c7f1bc4267adb
Daniel Velluto Bento <danielzinhobento11@gmail.com>
2019-09-18 06:04:37 -0300

    PHP2

--- Banco de Dados e PHP/tcc/proses-api.php
+++ Banco de Dados e PHP/tcc/proses-api.php
@@ -4,7 +4,7 @@
   header("Access-Control-Allow-Credentials: true");
   header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
   header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-  header("Content-Type: application/json; charset=utf-8");
+  header('Content-Type: text/html; charset=iso-8859-1');
 
   include "biblioteca/config.php";
 
@@ -12,68 +12,7 @@
   $today    = date('Y-m-d');
 
 
-  if($postjson['aksi']=='add'){
-
-  	$query = mysqli_query($mysqli, "INSERT INTO master_customer SET
-  		name_customer = '$postjson[name_customer]',
-  		desc_customer = '$postjson[desc_customer]',
-  		created_at	  = '$today'
-  	");
-
-  	$idcust = mysqli_insert_id($mysqli);
-
-  	if($query) $result = json_encode(array('success'=>true, 'customerid'=>$idcust));
-  	else $result = json_encode(array('success'=>false));
-
-  	echo $result;
-
-  }
-
-  elseif($postjson['aksi']=='getdata'){
-  	$data = array();
-  	$query = mysqli_query($mysqli, "SELECT * FROM master_customer ORDER BY customer_id DESC LIMIT $postjson[start],$postjson[limit]");
-
-  	while($row = mysqli_fetch_array($query)){
-
-  		$data[] = array(
-  			'customer_id' => $row['customer_id'],
-  			'name_customer' => $row['name_customer'],
-  			'desc_customer' => $row['desc_customer'],
-  			'created_at' => $row['created_at'],
-
-  		);
-  	}
-
-  	if($query) $result = json_encode(array('success'=>true, 'result'=>$data));
-  	else $result = json_encode(array('success'=>false));
-
-  	echo $result;
-
-  }
-
-  elseif($postjson['aksi']=='update'){
-  	$query = mysqli_query($mysqli, "UPDATE master_customer SET
-  		name_customer='$postjson[name_customer]',
-  		desc_customer='$postjson[desc_customer]' WHERE customer_id='$postjson[customer_id]'");
-
-  	if($query) $result = json_encode(array('success'=>true, 'result'=>'success'));
-  	else $result = json_encode(array('success'=>false, 'result'=>'error'));
-
-  	echo $result;
-
-  }
-
-  elseif($postjson['aksi']=='delete'){
-  	$query = mysqli_query($mysqli, "DELETE FROM master_customer WHERE customer_id='$postjson[customer_id]'");
-
-  	if($query) $result = json_encode(array('success'=>true, 'result'=>'success'));
-  	else $result = json_encode(array('success'=>false, 'result'=>'error'));
-
-  	echo $result;
-
-  }
-
-  elseif($postjson['aksi']=="login"){
+  if($postjson['aksi']=="login"){
     $password = md5($postjson['password']);
     $query = mysqli_query($mysqli, "SELECT * FROM usuario WHERE (login='$postjson[username]' AND senha='$password') OR (email='$postjson[username]' AND senha='$password')");
     $check = mysqli_num_rows($query);
@@ -130,6 +69,26 @@
     }
 
     echo $result;
+  }
+
+  elseif($postjson['aksi']=='getevento'){
+    $data = array();
+    $query = mysqli_query($mysqli, "SELECT * FROM evento where idUsuario='$postjson[idUsuario]' ORDER BY idEvento");
+
+    while($row = mysqli_fetch_array($query)){
+
+      $data[] = array(
+        'idEvento' => $row['idEvento'],
+        'NomeEvento' => $row['NomeEvento'],
+        'TipoEvento' => $row['Tipo']
+      );
+    }
+
+    if($query) $result = json_encode(array('success'=>true, 'result'=>$data));
+    else $result = json_encode(array('success'=>false));
+
+    echo $result;
+
   }
 
 
